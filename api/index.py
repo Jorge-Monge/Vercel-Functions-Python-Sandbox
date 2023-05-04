706d41b065bdb46a4c93d45a22e9495cb4de97c0
Jorge Monge <jorge.monge.blanco@outlook.com>
2023-05-03 22:30:17 -0600

    Changed the JSON response

--- api/index.py
+++ api/index.py
@@ -6,4 +6,4 @@ app = Sanic()
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
-    return json({'hello': path})
+    return json({'salutation': 'Hello, Mr. Faruqi', 'farewell': 'Good-bye'})
